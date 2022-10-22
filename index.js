f91db21174a2cdcd9a232a1d46b5c31ab6dcf5a1
gcaldev <gonzalomcalderon@hotmail.com>
2022-10-21 22:36:19 -0300

    feat(index): new date creation added

---
+++ index.js
@@ -0,0 +1,56 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+const isValidDate = (date) => date instanceof Date && !isNaN(date);
+
+const hasMillisecondsFormat = (date) => date.split("-").length === 1;
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get("/api/:date?", (req, res) => {
+  const incomingDate = req.params.date;
+  let date;
+  if(!incomingDate){
+    date = new Date();
+  } 
+  else if(hasMillisecondsFormat(incomingDate)){
+    date = new Date(parseInt(incomingDate));
+  }
+  else {
+    date = new Date(incomingDate);
+  }
+
+  if(isValidDate(date)){
+    const utc = date.toUTCString();
+    const unix = Date.parse(utc);
+
+    return res.status(200).json({ unix , utc });
+  }
+  return res.status(400).json({ error : "Invalid Date" });
+})
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
