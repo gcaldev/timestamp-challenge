6e9d1c2bb5ad5893fdfa88fd7790b393db4cc9d3
gcaldev <gonzalomcalderon@hotmail.com>
2022-10-22 17:41:02 -0300

    fix: used port fixed

--- index.js
+++ index.js
@@ -49,6 +49,6 @@ app.get("/api/:date?", (req, res) => {
 })
 
 // listen for requests :)
-var listener = app.listen(8080, function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
